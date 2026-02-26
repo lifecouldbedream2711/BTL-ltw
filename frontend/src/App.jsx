353b92037cf6856f2ec06fa249ae8476259c06f1
Pham-Quynh <Phampt987321@gmail.com>
2026-02-26 14:04:13 +0700

    cau truc thu muc

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,8 @@ import './App.css'
 
 function App() {
   return (
-    <div className="h-screen flex items-center justify-center bg-gray-100">
-      <button className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg">
+    <div className=" flex  bg-gray-100">
+      <button className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg w-50">
         Click me
       </button>
     </div>
---
+++ frontend/src/components/layout/PatientLayout.jsx
@@ -0,0 +1,6 @@
+function UserLayout(){
+    return <><h1 className="bg-red-300 text-center">Paracetamon</h1>
+    
+    </>
+}
+export default UserLayout;
